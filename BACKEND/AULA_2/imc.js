54ed9ae0b3f306da7048f7562acf2100abec6a18
GABRIELA DE SOUZA LIMA DE OLIVEIRA <gabriela.oliveira116@senaisp.edu.br>
2026-08-04 10:40:03 -0300

    04/08/2026

---
+++ BACKEND/AULA_2/imc.js
@@ -0,0 +1,25 @@
+//1.Importar a biblioteca para ler dados do terminal.
+const entrada = require("readline-sync")
+
+console.log("-------------------------");
+console.log("     CALCULADORA DE IMC      ")
+consoloe.log("------------------------\n");
+
+//2.Coleta de dados (Entrada)
+const nome = entrada.question("Qual o seu nome? ");
+const peso = entrada. questionFloat("Digite seu peso (ex: 70.5): ");
+const altura = entrada.questionFloat("Digite sua altura (ex: 1.75 ");
+
+//3. Calculo do IMC (processamento)
+//A formula e: peso dividido por (altuta vezes altura)
+;const imc = peso / (altura * altura)
+
+//4.Exibicao do resultado (Saida)
+consoloe.log("\n--------------------------");
+console.log(`Ola, ${nome}!`)
+console.log(`Seu peso: ${peso} kg`);
+console.log(`Sua altura: ${altura} m`);
+
+// O .toFixed(2) faz aparecer apenas 2 numeros apos o ponto
+console.log(`Seu IMC calculado e: ${imc.toFixed(2)}`)
+console.log("-----------------------------");
